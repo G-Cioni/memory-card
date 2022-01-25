790a4ea8b948e47e55245ae32d82d6f03efd48a7
Gianluca Cioni <gianlucacioni93@gmail.com>
2022-01-25 03:46:01 +0100

    Added Theme Provider with initial color palette

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  richBlack: '#101419',
+  steelTeal: '#468c98',
+  queenBlue: '#476C9B',
+  uranianBlue: '#ADD9F4',
+  cordovan: '#984447',
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
