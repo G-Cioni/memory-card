49e953dc5dc6bd1c64e9c3a5fd4ce3cefa9c0029
Gianluca Cioni <gianlucacioni93@gmail.com>
2022-01-25 04:08:00 +0100

    Added Css grid

--- src/styles/CardsStyled.js
+++ src/styles/CardsStyled.js
@@ -1,3 +1,9 @@
 import styled from 'styled-components';
 
-export const CardsStyled = styled.div``;
+export const CardsStyled = styled.div`
+  display: grid;
+  grid-gap: 2em;
+  grid-template-columns: repeat(4, 1fr);
+  margin: 1em;
+  max-width: 100em;
+`;
