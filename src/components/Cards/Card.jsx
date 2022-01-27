3effbb2b3531e9d2d150ac3d2adba06ecc1b1bb2
Gianluca Cioni <gianlucacioni93@gmail.com>
2022-01-27 01:41:47 +0100

    Imported example image

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { CardStyled } from '../../styles/CardStyled';
+import example from '../../images/example.jpg';
 
 function Card({ cardName, imgSource }) {
-  return <CardStyled src={imgSource} alt={cardName} />;
+  return <CardStyled src={example} alt={cardName} />;
 }
 
 export default Card;
