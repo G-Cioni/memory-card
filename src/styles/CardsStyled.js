505cc4c9e99bde35272223b1b7ad53f25511db5e
Gianluca Cioni <gianlucacioni93@gmail.com>
2022-01-27 01:48:37 +0100

    Centered Cards

--- src/styles/CardsStyled.js
+++ src/styles/CardsStyled.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 
 export const CardsStyled = styled.div`
+  align-items: center;
   display: grid;
   grid-gap: 2em;
   grid-template-columns: repeat(4, 1fr);
   margin: 1em;
-  max-width: 100em;
+  margin: 3em auto;
+  width: max-content;
 `;
