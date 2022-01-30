b41dcfacbae6954246dae94759841971d51b8d00
Gianluca Cioni <gianlucacioni93@gmail.com>
2022-01-30 04:56:30 +0100

    Added border-radius and box-shadow to Card

--- src/styles/CardStyled.js
+++ src/styles/CardStyled.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 export const CardStyled = styled.img`
+  border-radius: ${(props) => props.theme.borderRadius};
+  box-shadow: ${(props) => props.theme.boxShadow};
   height: 15em;
   justify-self: center;
   @media (max-width: 530px) {
