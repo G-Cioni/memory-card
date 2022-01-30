37c6a7825951897689888cef8a86a579e61476ee
Gianluca Cioni <gianlucacioni93@gmail.com>
2022-01-30 01:21:34 +0100

    Changed images export to object

--- src/helpers/images.js
+++ src/helpers/images.js
@@ -1,27 +1,76 @@
 import bulma from '../../src/images/bulma.jpg';
 import cell from '../../src/images/cell.jpg';
-import chichi from '../../src/images/chichi.jpg';
+import chiChi from '../../src/images/chi-chi.jpg';
 import frieza from '../../src/images/frieza.jpg';
 import gohan from '../../src/images/gohan.jpg';
 import goku from '../../src/images/goku.jpg';
 import goten from '../../src/images/goten.jpg';
-import majinBuu from '../../src/images/majinBuu.jpg';
+import majinBuu from '../../src/images/majin-buu.jpg';
 import mrSatan from '../../src/images/mr-satan.jpg';
-import piccolo from '../../src/images/piccolo.jpg';
+import junior from '../../src/images/junior.jpg';
 import trunks from '../../src/images/trunks.jpg';
 import vegeta from '../../src/images/vegeta.jpg';
+import uniqid from 'uniqid';
 
-export const images = [
-  bulma,
-  cell,
-  chichi,
-  frieza,
-  gohan,
-  goku,
-  goten,
-  majinBuu,
-  mrSatan,
-  piccolo,
-  trunks,
-  vegeta,
-];
+export const images = {
+  bulma: {
+    name: 'Bulma',
+    image: bulma,
+    id: uniqid(),
+  },
+  cell: {
+    name: 'Cell',
+    image: cell,
+    id: uniqid(),
+  },
+  chiChi: {
+    name: 'Chi-Chi',
+    image: chiChi,
+    id: uniqid(),
+  },
+  frieza: {
+    name: 'Frieza',
+    image: frieza,
+    id: uniqid(),
+  },
+  gohan: {
+    name: 'Gohan',
+    image: gohan,
+    id: uniqid(),
+  },
+  goku: {
+    name: 'Goku',
+    image: goku,
+    id: uniqid(),
+  },
+  goten: {
+    name: 'Goten',
+    image: goten,
+    id: uniqid(),
+  },
+  majinBuu: {
+    name: 'Majin Buu',
+    image: majinBuu,
+    id: uniqid(),
+  },
+  mrSatan: {
+    name: 'Mr. Satan',
+    image: mrSatan,
+    id: uniqid(),
+  },
+  junior: {
+    name: 'Junior',
+    image: junior,
+    id: uniqid(),
+  },
+  trunks: {
+    name: 'Trunks',
+    image: trunks,
+    id: uniqid(),
+  },
+  vegeta: {
+    name: 'Vegeta',
+    image: vegeta,
+    id: uniqid(),
+  },
+};
