8eebdbcd0f35840358a67b9c3fb7ba05ed7a8f8e
Gianluca Cioni <gianlucacioni93@gmail.com>
2022-01-31 01:17:01 +0100

    added width, padding and object-fit

--- src/styles/CardStyled.js
+++ src/styles/CardStyled.js
@@ -4,6 +4,9 @@ export const CardStyled = styled.img`
   border-radius: ${(props) => props.theme.borderRadius};
   box-shadow: ${(props) => props.theme.boxShadow};
   height: 15em;
+  width: 10em;
+  padding: 0.5em;
+  object-fit: contain;
   justify-self: center;
   @media (max-width: 530px) {
     height: 13em;
