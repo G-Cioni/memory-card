ccd946f12b1cbcdd65928cb3dedd85c54c045849
Gianluca Cioni <gianlucacioni93@gmail.com>
2022-01-31 01:16:40 +0100

    Changed responsive behavior

--- src/styles/CardsStyled.js
+++ src/styles/CardsStyled.js
@@ -4,18 +4,19 @@ export const CardsStyled = styled.div`
   align-items: center;
   display: grid;
   grid-gap: 2em;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(6, 1fr);
   margin: 1em;
   margin: 2em auto;
   width: max-content;
 
-  @media (max-width: 1060px) {
+  @media (max-width: 1130px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+  @media (max-width: 745px) {
     grid-template-columns: repeat(3, 1fr);
+    grid-gap: 1em;
   }
-  @media (max-width: 780px) {
+  @media (max-width: 535px) {
     grid-template-columns: repeat(2, 1fr);
-  }
-  @media (max-width: 530px) {
-    grid-gap: 1.2em;
   }
 `;
